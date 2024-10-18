6428bde2b80429a8a19a0efe631cb532f1b66964
ssodboeva <radnaevas87@gmail.com>
2024-10-18 14:37:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from fake_math import divide as fake_divide
+from true_math import divide as true_divide
+
+result_1 = fake_divide (24, 2)
+result_2 = fake_divide (12,0)
+result_3 = true_divide (30, 15)
+result_4 = true_divide (9, 0)
+
+print (result_1)
+print (result_2)
+print (result_3)
+print (result_4)
